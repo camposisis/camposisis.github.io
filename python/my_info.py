44b45bdc19f6157e1cc34a0fa307cac7fa71e3b1
User <user@vagelos-ve735-0882.apnd.wlan.private.upenn.edu>
2018-06-05 11:55:10 -0400

    again

--- python/my_info.py
+++ python/my_info.py
@@ -1,4 +1,1 @@
-my_info = {"first_name":"Isis",last_name:"Campos", hometown: "Los Angeles", favorite_food:"pizza"}
-
-
-}
+my_info = {"first_name":"Isis", "last_name":"Campos", "hometown": "Los Angeles", "favorite_food":"pizza"}
