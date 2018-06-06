16ceb2a83b141c1f8e87fff752da4d6c26a28de2
User <user@vagelos-ve735-0882.apnd.wlan.private.upenn.edu>
2018-06-06 11:01:48 -0400

    added all stuff

---
+++ python/years.py
@@ -0,0 +1,7 @@
+origin = 2000
+if origin > 0 and origin < 1900:
+  print("Please tell us about the past")
+elif origin  > 1900 and origin < 2020:
+  print("hello neighbor")
+else:
+  print("describe the future")
