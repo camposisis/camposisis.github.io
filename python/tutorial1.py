de588ea2684da04ab4c9386d7ef5c47fbc0bc12c
User <user@vagelos-ve735-0882.apnd.wlan.private.upenn.edu>
2018-06-07 10:53:48 -0400

    added practice codes

--- python/tutorial1.py
+++ python/tutorial1.py
@@ -27,3 +27,48 @@ print(names)
 
 a_row = [3,'fox','eliza','SO COOL']
 print(names + a_row)
+
+with open('practice.csv', 'w') as fout:
+    csvwriter = csv.writer(fout)
+    for i in range(0, 100, 10):
+        csvwriter.writerow([i, i + 5, i + 15, i + 20])
+
+with open('practice.csv', 'r') as fin:
+    our_reader = csv.reader(fin)
+    data = [row for row in our_reader]
+print(data)
+
+data [3][1] = 'Ethan'
+data [4][1] = 'Brandon'
+data [2][1] = 'Tony the cat'
+
+with open ('practice.csv', 'w') as fout:
+    csvwriter = csv.writer(fout)
+    for row in data:
+        csvwriter.writerow(row)
+
+print(data)
+
+groceries = []
+with open('shopping.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        groceries.append(row)
+
+print(groceries)
+
+print(groceries[2]['name'])
+
+for item in groceries:
+    print(item['name'])
+
+tony = {'name':'tony', 'nutritious': 'False', 'id': '5' }
+
+groceries.append(tony)
+
+with open ('updated_groceries.csv', 'w') as fout:
+    column_names = ['id', 'name', 'nutritious']
+    dictwriter = csv. DictWriter(fout, fieldnames=column_names)
+    dictwriter.writeheader()
+    for row in groceries:
+        dictwriter.writerow(row)
